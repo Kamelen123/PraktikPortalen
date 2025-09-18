b714352f0bc1dbd13f823d31f5d6a4f6725b554f
Torbjörn <torbjorn.rodstrom@gmail.com>
2025-09-18 12:04:20 +0200

    Added repository for Company

---
+++ PraktikPortalen.Domain/Interfaces/Repositories/ICompanyRepository.cs
@@ -0,0 +1,15 @@
+﻿using PraktikPortalen.Domain.Entities;
+
+namespace PraktikPortalen.Domain.Interfaces.Repositories
+{
+    public interface ICompanyRepository
+    {
+        Task<List<Company>> GetAllAsync(CancellationToken ct = default);
+        Task<Company?> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<bool> ExistsAsync(int id, CancellationToken ct = default);
+        Task AddAsync(Company entity, CancellationToken ct = default);
+        Task UpdateAsync(Company entity, CancellationToken ct = default);
+        Task DeleteAsync(Company entity, CancellationToken ct = default);
+        Task<bool> SaveChangesAsync(CancellationToken ct = default);
+    }
+}
